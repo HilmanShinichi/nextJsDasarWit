e7cec728d2f63beccfc661ddf3b73c537647b102
Hilman Shinichi <hilmanseptiana19@gmail.com.com>
2023-06-19 22:01:20 +0700

    eps 5

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,7 @@
 export default function Blog() {
     return (
         <div>
-            <p>fBlog</p>
+            <p>Blog</p>
         </div>
     )
-}
+} 
---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+
+import styles from '../app/page.module.css'
+
+export default function Home() {
+  return (
+<div>image</div>
+  )
+}
