0219a917e5a51919347cee026f71ed238a4da4e7
Hilman Shinichi <hilmanseptiana19@gmail.com.com>
2023-06-21 21:28:57 +0700

    image

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import RootLayout from "../app/layout";
 import style from "../styles/home.module.css";
+import Image from "next/image";
 
 
 export default function Home() {
   return (
     <RootLayout pageTitle='Home Page'>
+      <Image src="/profile.png" alt="profile" width={200} height={200}/>
       <h1 className={style["title-homepage"]}>Welcome Mufid</h1>
     </RootLayout>
   );
